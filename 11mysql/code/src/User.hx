02eae5fd1989d5f013c10c7305d6c13675d4b938
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-10-21 17:25:00 +0200

    refactor

--- 11mysql/code/src/User.hx
+++
@@ -1,9 +0,0 @@
-import sys.db.Types;
-import sys.db.*;
-
-class User extends sys.db.Object {
-	public var id : SId;
-	public var name : SString<32>;
-	public var birthday : SDate;
-	public var phoneNumber : SNull<SText>;
-}
